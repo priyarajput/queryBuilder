5fed18542fb1feadaf62ad6a11a22f0f8022b45f
priya.verma <priya.verma@decimal.co.in>
2019-12-11 16:33:26 +0530

    update input text functionality on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,12 @@ export class AppComponent {
   title = 'queryBuilder';
 
   public query = {
-    rules: [
-      { field: 'Col1', value: "1" },
-      { field: 'Col2', value: "2" }
-    ]
+    rules: [{ "id": "Col1", "field": "Col1", "value": "1" }, { "id": "Col2", "field": "Col2", "value": "2" }, { "rules": [{ "id": "Col3", "field": "Col3 56", "value": "3 56" }] }]
   };
 
   public config = {
     fields: {
-      col3: { field: 'Col3',value: "3" },
+      col3: { id: 'Col3', field: 'Col3', value: "3" },
     }
   }
 
--- src/app/components/query-builder/query-builder.component.ts
+++ src/app/components/query-builder/query-builder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges, ViewChildren, QueryList, ElementRef, ViewChild, Renderer, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-query-builder',
@@ -9,16 +9,27 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
   @Input() data = { rules: [] }
   @Input() config = { fields: {} }
   @Input() allowRuleset: boolean = false;
-  @Input() parentData = this.data;
+  @Input() parentData: any;
+  @ViewChildren('attendee') attendeeInputs: QueryList<ElementRef>;
+  @Output() getId = new EventEmitter<any>();
+
+  setId = 0;
 
   public fields = {};
 
-  constructor() {
+  constructor(private renderer: Renderer) {
   }
 
 
   ngOnInit() {
     console.log("ng on init")
+  }
+
+  ngAfterViewInit(): void {
+    console.log("attendeeInputs", this.attendeeInputs)
+    this.attendeeInputs.forEach((s: ElementRef, i) => {
+      console.log(s.nativeElement, i)
+    })
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -44,6 +55,7 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
     let parent = this.data;
     const field = this.fields[0];
     parent.rules = parent.rules.concat([{
+      id: field.id + " " + addRandomId,
       field: field.field + " " + addRandomId,
       value: field.value + " " + addRandomId
     }]);
@@ -57,7 +69,6 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
   addRuleSet(): void {
     let parent = this.data;
     parent.rules = parent.rules.concat([{ rules: [] }]);
-
   }
 
   removeRuleSet(): void {
@@ -69,5 +80,47 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
     return {
       $implicit: "A ruleset cannot be empty. Please add a rule or remove it all together."
     };
+  }
+
+  onClick(event: string, field?: object) {
+    console.log(field, event, this.data)
+    this.setId = field['id'];
+    console.log("id", this.setId);
+
+    this.getId.emit(this.setId);
+  }
+
+  valueChange(e) {
+    console.log("valueChange", e);
+    this.setId = e;
+    this.getId.emit(this.setId);
+    // this.attendeeInputs.forEach((s: ElementRef, i) => {
+    //   console.log(s.nativeElement, i)
+    //   // this.renderer.setElementStyle(s.nativeElement, "background", "yellow");
+    //   // this.renderer.setProperty(this.zipEl.nativeElement, 'value', '94085');
+    // });
+
+  }
+  updateValue() {
+    console.log("updateValue", this.setId);
+    this.search(this.data.rules);
+    this.attendeeInputs.forEach((s: ElementRef, i) => {
+      // console.log(s.nativeElement, i)
+      this.renderer.setElementStyle(s.nativeElement, "background", "yellow");
+      // this.renderer.setProperty(this.zipEl.nativeElement, 'value', '94085');
+    });
+  }
+
+  search(rules) {
+    rules.forEach((element, index) => {
+      if (element.id === this.setId) {
+        console.log("found", element);
+        element.value = "Update some text" + this.addRandomId();
+        return false
+      }
+      if (element.rules) {
+        this.search(element.rules);
+      }
+    });
   }
 }
