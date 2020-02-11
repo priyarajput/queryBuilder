b1a1df762f81d58616fe155cc289cc9e356ba9bf
priya.verma <priya.verma@decimal.co.in>
2020-02-11 13:17:11 +0530

    fixed iisue

--- src/app/components/query-builder/query-builder.component.ts
+++ src/app/components/query-builder/query-builder.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input, SimpleChanges, OnChanges, ViewChildren, QueryList, ElementRef, ViewChild, Renderer, Output, EventEmitter } from '@angular/core';
+import {
+  Component, OnInit, Input, SimpleChanges, OnChanges,
+  ViewChildren, QueryList, ElementRef, ViewChild, Renderer, Output, EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'app-query-builder',
@@ -6,10 +9,11 @@ import { Component, OnInit, Input, SimpleChanges, OnChanges, ViewChildren, Query
   styleUrls: ['./query-builder.component.scss']
 })
 export class QueryBuilderComponent implements OnChanges, OnInit {
-  @Input() data = { rules: [] }
-  @Input() config = { fields: {} }
-  @Input() allowRuleset: boolean = false;
+  @Input() data = { rules: [] };
+  @Input() config = { fields: {} };
+  @Input() allowRuleset = false;
   @Input() parentData: any;
+  @Input() indexRuleSet: number;
   @ViewChildren('attendee') attendeeInputs: QueryList<ElementRef>;
   @Output() getId = new EventEmitter<any>();
 
@@ -24,18 +28,19 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
 
 
   ngOnInit() {
-    console.log("ng on init")
+    console.log('ng on init');
   }
 
+  // tslint:disable-next-line: use-lifecycle-interface
   ngAfterViewInit(): void {
-    console.log("attendeeInputs", this.attendeeInputs)
+    console.log('attendeeInputs', this.attendeeInputs);
     this.attendeeInputs.forEach((s: ElementRef, i) => {
-      console.log(s.nativeElement, i)
-    })
+      console.log(s.nativeElement, i);
+    });
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    console.log("ng On change")
+    console.log('ng On change');
     const config = this.config;
     const type = typeof config;
     if (type === 'object') {
@@ -54,44 +59,43 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
 
   addRule(): void {
     const addRandomId = this.addRandomId();
-    let parent = this.data;
+    const parent = this.data;
     const field = this.fields[0];
     parent.rules = parent.rules.concat([{
-      id: field.id + " " + addRandomId,
-      field: field.field + " " + addRandomId,
-      value: field.value + " " + addRandomId
+      id: field.id + ' ' + addRandomId,
+      field: field.field + ' ' + addRandomId,
+      value: field.value + ' ' + addRandomId
     }]);
   }
 
   removeRule(rule): void {
-    let parent = this.data;
-    parent.rules = parent.rules.filter((value, index) => index != rule);
+    const parent = this.data;
+    parent.rules = parent.rules.filter((value, index) => index !== rule);
   }
 
   addRuleSet(): void {
-    let parent = this.data;
+    const parent = this.data;
     parent.rules = parent.rules.concat([{ rules: [] }]);
   }
 
   removeRuleSet(): void {
-    let parent = this.parentData;
-    parent.rules = parent.rules.filter(r => r !== this.data)
+    this.parentData.splice(this.indexRuleSet, 1);
   }
 
   getEmptyWarningContext(): object {
     return {
-      $implicit: "A ruleset cannot be empty. Please add a rule or remove it all together."
+      $implicit: 'A ruleset cannot be empty. Please add a rule or remove it all together.'
     };
   }
 
   valueChange(e) {
     this.setId = e.setId;
-    this.charAt = e.charAt
+    this.charAt = e.charAt;
 
     const d = {
       charAt: this.charAt,
       setId: this.setId
-    }
+    };
     this.getId.emit(d);
   }
 
@@ -105,7 +109,7 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
     const d = {
       charAt: this.charAt,
       setId: this.setId
-    }
+    };
     this.getId.emit(d);
   }
 
@@ -113,8 +117,8 @@ export class QueryBuilderComponent implements OnChanges, OnInit {
     rules.forEach((element, index) => {
       if (element.id === this.setId) {
         element.value = [element.value.slice(0, this.charAt),
-        "Update some text" + this.addRandomId(), element.value.slice(this.charAt)].join('');
-        return false
+        'Update some text ' + this.addRandomId(), element.value.slice(this.charAt)].join('');
+        return false;
       }
       if (element.rules) {
         this.search(element.rules);
